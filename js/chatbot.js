4ce680a2e7bf34ce66b75ff65f4a8e3cfce72857
Karolina Barrera <karolinabarod@gmail.com>
2021-02-12 21:03:32 -0600

    Prueba ChatBot

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,18 +1,19 @@
-var open = true;
+var open = false;
+document.getElementById("Chatbot-container").style.display = "none";
 document.getElementById("chat").style.display = "none";
 
 function abrirChatBox() {
     if(open){
-        document.getElementById("Chatbot").style.display = "none";
+        document.getElementById("Chatbot-container").style.display = "none";
         open = false;
     }else{
-        document.getElementById("Chatbot").style.display = "block";
+        document.getElementById("Chatbot-container").style.display = "block";
         open = true;
     } 
 }
 
 function cerrarChatBox(){
-    document.getElementById("Chatbot").style.display = "none";
+    document.getElementById("Chatbot-container").style.display = "none";
     open = false;
 }
 
@@ -27,7 +28,7 @@ function regresar(){
 }
 
 function scrollBtn () {  
-    var chatBox = document.getElementById("Chatbot").scrollTop;
+    var chatBox = document.getElementById("Chatbot-container").scrollTop;
     var btnCerrar = document.getElementById("cerrar");
 
     chatBox > 0 ? btnCerrar.style.backgroundColor =  'rgba(0, 0, 0, 0.3)' : btnCerrar.style.backgroundColor =  'rgba(0, 0, 0, 0)';
